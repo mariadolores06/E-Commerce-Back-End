2d9d4183392df1e389e288d7adc3826f37ed45c3
unknown <maria.rodriguez@northwestern.edu>
2023-05-26 23:11:37 -0500

    finished routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
+const sequelize = require('../../config/connection.js');
 
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,29 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
+const sequelize = require('../../config/connection.js');
+
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  const products = await Product.findAll({
+    include: [Category, {model: Tag, through: ProductTag}] 
+  })
+  res.status(200).json(products)
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  const product = await Product.findOne({
+    where: {id: req.params.id},
+    include: [Category, {model: Tag, through: ProductTag}] 
+  })
+  res.status(200).json(product)
 });
 
 // create new product
@@ -89,8 +100,14 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  await Product.destroy({
+    where: {
+      id: req.params.id 
+    }
+  })
+  res.status(200).json('Deleted')
 });
 
 module.exports = router;
